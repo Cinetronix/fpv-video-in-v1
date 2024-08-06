a36ab08e825cb114cc95833854b89386ad2629d4
auster <iAuster@ya.ru>
2024-08-06 15:38:19 +0100

    Add TC9590 implementation

---
+++ TC9590XBG-PCB/panel.bat
@@ -0,0 +1,10 @@
+kikit panelize ^
+    --layout "grid; rows: 1; cols: 3; space: 5mm;" ^
+    --tabs "fixed; width: 5mm;" ^
+    --cuts "mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm" ^
+    --framing "railstb; width: 5mm; space: 3mm; mintotalheight: 72mm; mintotalwidth: 72mm" ^
+    --tooling "3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm" ^
+    --fiducials "3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;" ^
+    --text "simple; text: Cinetronix Video input v1.1a; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;" ^
+    --post "millradius: 1mm" ^
+    fpv-hdmi-v2.kicad_pcb Panel/panel.kicad_pcb
